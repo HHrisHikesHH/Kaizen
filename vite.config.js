fcac8f7a02b7f8713dbe2f0b65fe2a276301b1de
HHrisHikesHH <hrishikeshkalekinge31@gmail.com>
2026-03-08 16:03:42 +0530

    Update Vite configuration to change base path for production build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
-  base: command === 'build' ? '/kaizen/' : '/',
+  base: command === 'build' ? '/Kaizen/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
